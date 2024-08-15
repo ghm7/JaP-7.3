f53a1ebe5dc10471fec1355ccf357bec9444ac5f
rodrituca <ezecapybara@gmail.com>
2024-08-15 09:29:35 -0300

    validacion de campos vacios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+var sumbit = document.getElementById("regBtn")
+sumbit.addEventListener("click", function (event) {
+event.preventDefault()
+
+var passwords = document.querySelectorAll("input[type=password]");
+var inputs = document.querySelectorAll("input");
+var email = inputs[0];
+var name = inputs[1];
+var surname= inputs[2];
+
+for (var i=0; i<inputs.length; i++)
+    if (inputs [i].value === "") {
+    showAlertError();
+    return false;
+    }
+
+//if (passwords[0].value.length < 6) 
+})
